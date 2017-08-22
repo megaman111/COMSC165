e447c35a98710fdf40c21f25f4d4c56a9926918a
Joe Gertler <jojogertler@gmail.com>
2017-08-22 22:06:37 +0000

    First commit

---
+++ gradebook_lowestdropped/gradebook_lowestdropped.cpp
@@ -0,0 +1,17 @@
+///****************************************************************************
+/// Description: This program allow the user to enter each student’s
+/// name and his or her four test scores, and should drop each student’s 
+/// lowest score, then calculate and display each student’s average test score
+/// and then, give a letter grade based on the average
+/// Author: Joseph Gertler
+/// COMSC 165 Section 3020
+/// Date: August 22, 2017
+/// Status : Incomplete 
+///*****************************************************************************
+#include <iostream>
+using namespace std;
+
+int main() {
+///*************************DATA CODE BLOCK*************************************
+
+}
