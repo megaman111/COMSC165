7fab962601c6262debba0f351827e063fa48c9dd
Joe Gertler <jojogertler@gmail.com>
2017-08-24 23:24:04 +0000

    started work on lab 1

--- Labs/gradebook_lowestdropped/gradebook_lowestdropped.cpp
+++ Labs/gradebook_lowestdropped/gradebook_lowestdropped.cpp
@@ -10,9 +10,60 @@
 /// Status : Incomplete 
 ///*****************************************************************************
 #include <iostream>
+#include <iomanip>
 using namespace std;
-
 int main() {
-///*************************DATA CODE BLOCK*************************************
+///*************************CONSTANTS*******************************************
+const int NUM_GRADES = 5;
+const int NUM_STUDENTS = 5;
 
+
+///*************************FUNCTION PROTO CODE BLOCK***************************
+void dropLowestScore();
+double returnAverage(double test_score_1,double test_score_2, double test_score_3,double test_score_4,double test_score_5);
+void displayGrade();
+string askUserNames(string student_names[]);
+void format();
+///*************************DATA CODE BLOCK*************************************
+string student_names[NUM_STUDENTS];
+char letter_grade[NUM_GRADES] = {'A','B','C','D','F'};
+
+///*************************Processing******************************************
+askUserNames(student_names);
+cout << "Student #1: " << student_names[0] << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+cout << "Student #2: " << student_names[1] << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+cout << "Student #3: " << student_names[2] << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+cout << "Student #4: " << student_names[3] << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+cout << "Student #5: " << student_names[4] << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+}
+///*************************FUNCTION CODE BLOCK*********************************
+string askUserNames(string student_names[]){
+    cout << "Enter name of student #1: ";
+    getline(cin, student_names[0]);
+    cout << "Enter name of student #2: ";
+    getline(cin, student_names[1]);
+    cout << "Enter name of student #3: ";
+    getline(cin, student_names[2]);
+    cout << "Enter name of student #4: ";
+    getline(cin, student_names[3]);
+    cout << "Enter name of student #5: ";
+    getline(cin, student_names[4]);
+    return student_names[0,1,2,3,4];
+}
+double returnAverage(double test_score_1[], double test_score_2[], double test_score_3[], double test_score_4[], double test_score_5[]){
+}
+void format(int studentnum){
+cout << "Student #" << studentnum << ": " << endl;
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
 }
