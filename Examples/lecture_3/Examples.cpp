74036fded56f9140c937d70e9bb26b8e218282ef
Joe Gertler <jojogertler@gmail.com>
2017-08-24 21:32:54 +0000

    added examples for third lecture?

---
+++ Examples/lecture_3/Examples.cpp
@@ -0,0 +1,14 @@
+//Example of indirection operator: https://hastebin.com/cayajozoyo.cpp
+//Example of subscript notation with a pointer variable and pointer notation with an array name: https://hastebin.com/umowiyeyij.cpp
+//Example of pointer to display the contents of an array: https://hastebin.com/ciqemodabo.cpp
+//Example of a pointer to display the contents : https://hastebin.com/ixupaculuw.cpp
+//Example of pointers as function parameters: https://hastebin.com/azapojiyuz.cpp
+//Example of  Dynamic Memory Allocation: https://hastebin.com/eletatumuk.cpp
+//Example of  Returning Pointers from Functions: https://hastebin.com/mevowocopi.cpp
+//Example of pointers to avoid memory leaks: https://hastebin.com/iwojilakig.cpp
+//???: https://hastebin.com/ebokuzehec.cpp
+#include<iostream>
+using namespace std;
+int main(){
+cout << "Used for testing only, ignore this output statement, Thanks!" << endl;
+}
