3c5500798871d2bb2657e869140957d34daa8b9d
Joe Gertler <jojogertler@gmail.com>
2017-09-05 22:42:05 +0000

    finished sorting benchmarks

--- Labs/sortingbenchmarks.cpp
+++ Labs/sortingbenchmarks.cpp
@@ -6,29 +6,38 @@
 /// Status : Incomplete 
 ///****************************************************************************
 #include <iostream>
+#include <iomanip>
 #include <string>
 //Function prototypes
 void bubbleSortArray(int array[], int size);
 void showArray(const int [], int);
 void selectionSortArray(int [], int);
+void format();
 using namespace std;
 int main() {
     int tensortone[10] = {39,72,46,96,59,7,47,33,2,69};
     int tensorttwo [10] = {39,72,46,96,59,7,47,33,2,69};
     int hundredsortone[100] = {32,70,75,73,23,48,43,12,11,64,16,66,3,98,73,20,10,80,25,3,44,31,73,82,81,25,9,22,97,27,12,66,68,47,91,77,84,2,90,66,26,25,24,33,22,43,40,72,8,60,55,88,78,35,47,64,38,7,79,98,85,7,73,68,32,62,87,10,89,17,83,75,25,10,68,46,73,31,69,72,25,11,6,63,18,10,76,36,4,66,15,30,88,52,80,3,2,30,38,89};
     int hundredsorttwo[100] = {32,70,75,73,23,48,43,12,11,64,16,66,3,98,73,20,10,80,25,3,44,31,73,82,81,25,9,22,97,27,12,66,68,47,91,77,84,2,90,66,26,25,24,33,22,43,40,72,8,60,55,88,78,35,47,64,38,7,79,98,85,7,73,68,32,62,87,10,89,17,83,75,25,10,68,46,73,31,69,72,25,11,6,63,18,10,76,36,4,66,15,30,88,52,80,3,2,30,38,89};
+
+    
+    format();
     bubbleSortArray(tensortone,10);
-    selectionSortArray(tensorttwo,10);
-    bubbleSortArray(hundredsortone, 100);
-    selectionSortArray(hundredsorttwo, 100);
     cout << "Bubble Sorted Array of 10: ";
     showArray(tensortone,10);
+    format();
+    selectionSortArray(tensorttwo,10);
     cout << "Selection Sorted Array of 10: ";
     showArray(tensorttwo, 10);
+    format();
+    bubbleSortArray(hundredsortone, 100);
     cout << "Bubble Sorted Array of 100: ";
     showArray(hundredsortone, 100);
+    format();
+    selectionSortArray(hundredsorttwo, 100);
     cout << "Selection Sorted Array of 100: ";
     showArray(hundredsorttwo, 100);
+    format();
     
     
 }   
@@ -52,7 +61,7 @@ void bubbleSortArray(int array[], int size)
                 swapCount++;
             }
         }
-    }
+    }   
   cout << "Bubble Sort Swap Count: " << swapCount << endl;
   cout <<"Bubble Sort Compare Count: " << compareCount << endl;
 }
@@ -91,4 +100,8 @@ void selectionSortArray(int array[], int size)
   }
   cout << "Selection Sort Swap Count: " << swapCount << endl;
   cout <<"Selection Sort Compare Count: " << compareCount << endl;
-}   
+}
+void format(){
+cout << setfill('_') << setw(80) << endl;
+cout << " " << endl;
+}
