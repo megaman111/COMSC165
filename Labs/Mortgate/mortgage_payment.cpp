df6e6f7283a0ee285020e9aae91527e47a92b5c1
Joe Gertler <jojogertler@gmail.com>
2017-10-18 19:35:25 +0000

    updated mortgage lab to actually use the classes properly

--- Labs/Mortgate/mortgage_payment.cpp
+++ Labs/Mortgate/mortgage_payment.cpp
@@ -9,21 +9,22 @@ class Mortgage{
     int years;
     int term;
     int payment;
+    double insertMortgage(void){
+        Mortgage newMortgage;
+        cout << "Enter the amount of the loan: ";
+        cin >> newMortgage.loan;
+        cout << "Enter the annual interest rate in decimal form (Ex .075): ";
+        cin >> newMortgage.rate;
+        cout << "Enter the length of the loan in years: ";
+        cin >> newMortgage.years;
+        newMortgage.term = pow(1+newMortgage.rate/12, 12*newMortgage.years);
+        newMortgage.payment = newMortgage.loan * (newMortgage.rate / 12) * newMortgage.term / (newMortgage.term - 1);
+        cout << "Monthly Payment: " << newMortgage.payment / 12 << endl;
+        cout << "Total Payment: " << newMortgage.payment << endl;
+}
 };
-double insertMortgage();
 int main(){
-    insertMortgage();
+    Mortgage finalMortgage;
+    finalMortgage.insertMortgage();
 }
-double insertMortgage(){
-    Mortgage newMortgage;
-    cout << "Enter the amount of the loan: ";
-    cin >> newMortgage.loan;
-    cout << "Enter the annual interest rate in decimal form (Ex .075): ";
-    cin >> newMortgage.rate;
-    cout << "Enter the length of the loan in years: ";
-    cin >> newMortgage.years;
-    newMortgage.term = (1 + newMortgage.rate/12)*pow(12, newMortgage.years);
-    newMortgage.payment = newMortgage.loan * (newMortgage.rate / 12) * newMortgage.term / (newMortgage.term - 1);
-    cout << "Monthly Payment: " << newMortgage.payment / 12 << endl;
-    cout << "Total Payment: " << newMortgage.payment << endl;
-}
+
